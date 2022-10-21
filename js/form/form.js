8409b117353c4436559af3d5b068fd088b59d25f
goregora <104126170+goregora@users.noreply.github.com>
2022-10-21 22:05:19 +0300

    refactor

--- js/form/form.js
+++ js/form/form.js
@@ -12,11 +12,21 @@ const photoPreview = form.querySelector('.img-upload__preview');
 const scaleControlReduceButton = form.querySelector('.scale__control--smaller');
 const scaleControlAddButton = form.querySelector('.scale__control--bigger');
 const scaleControlInputValue = form.querySelector('.scale__control--value');
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__text',
+  errorTextParent: 'img-upload__text'
+});
 
+/**
+ * Функция устанавливает значение масштаба по умолчанию в окне редактора загруженного изображения.
+ */
 function setDefaultScaleValue() {
   scaleControlInputValue.value = `${DEFAULT_SCALE_VALUE}%`;
 }
 
+/**
+ * Функция увеличивает значение масштаба в окне редактора загруженного изображения.
+ */
 function setAddedScaleValue() {
   let currentValue = Number((scaleControlInputValue.value).slice(0, -1));
 
@@ -27,6 +37,9 @@ function setAddedScaleValue() {
   photoPreview.style.transform = `scale(${currentValue / 100})`;
 }
 
+/**
+ * Функция уменьшает значение масштаба в окне редактора загруженного изображения.
+ */
 function setReducedScaleValue() {
   let currentValue = Number((scaleControlInputValue.value).slice(0, -1));
 
@@ -37,44 +50,27 @@ function setReducedScaleValue() {
   photoPreview.style.transform = `scale(${currentValue / 100})`;
 }
 
-function addEventListenersToForm() {
-  function openEditorModal() {
-    editorModal.classList.remove('hidden');
-    document.body.classList.add('modal-open');
-    setDefaultScaleValue();
-  }
-
-  function closeEditorModal() {
-    editorModal.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-  }
-
-  function isEscapeKey(evt) {
-    return (evt.key === 'Escape');
-  }
-
-  uploadControl.addEventListener('change', () => {
-    openEditorModal();
-  });
-  // openEditorModal();
-
-  closeEditorModalButton.addEventListener('click', () => {
+/**
+ * Функция вызывает закрытие окна редактора загруженного изображения, если произошло нажатие ESC.
+ * @param {Object} evt - объект события.
+ */
+function closeEditorModalIfEscapeKey(evt) {
+  if (isEscapeKey(evt)) {
     closeEditorModal();
-  });
+  }
+}
 
-  document.addEventListener('keydown', (evt) => {
-    if (isEscapeKey(evt)) {
-      closeEditorModal();
-    }
-  });
-
-  scaleControlReduceButton.addEventListener('click', () => {
-    setReducedScaleValue();
-  });
-
-  scaleControlAddButton.addEventListener('click', () => {
-    setAddedScaleValue();
-  });
+/**
+ * Функция открывает окно редактора загруженного изображения.
+ */
+function openEditorModal() {
+  closeEditorModalButton.addEventListener('click', closeEditorModal);
+  scaleControlReduceButton.addEventListener('click', setReducedScaleValue);
+  scaleControlAddButton.addEventListener('click', setAddedScaleValue);
+  document.addEventListener('keydown', closeEditorModalIfEscapeKey);
+  editorModal.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  setDefaultScaleValue();
 
   let effectName;
   form.addEventListener('change', (evt) => {
@@ -84,31 +80,53 @@ function addEventListenersToForm() {
       effectName = evt.target.value;
     }
   });
+
+  form.addEventListener('submit', (evt) => {
+    if (!pristine.validate()) {
+      evt.preventDefault();
+    }
+  });
+
+  pristine.addValidator(
+    form.querySelector('.text__description'),
+    isCommentLengthCorrect,
+    `от ${MIN_COMMENT_LENGTH} до ${MAX_COMMENT_LENGTH} символов`
+  );
 }
 
-const pristine = new Pristine(form, {
-  classTo: 'img-upload__text',
-  errorTextParent: 'img-upload__text'
-});
-
-
-function isCommentLengthCorrect (value) {
-  return value.length >= MIN_COMMENT_LENGTH && value.length <= MAX_COMMENT_LENGTH;
+/**
+ * Функция закрывает окно редактора загруженного изображения.
+ */
+function closeEditorModal() {
+  editorModal.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  uploadControl.value = '';
+  document.removeEventListener('keydown', closeEditorModalIfEscapeKey);
 }
 
-pristine.addValidator(
-  form.querySelector('.text__description'),
-  isCommentLengthCorrect,
-  `от ${MIN_COMMENT_LENGTH} до ${MAX_COMMENT_LENGTH} символов`
-);
+/**
+ * Функция проверяет является ли нажатая клавиша ESC-ом.
+ * @param {Object} evt - объект события.
+ * @return {boolean} - истина, если нажатая клавиша - ESC.
+ */
+function isEscapeKey(evt) {
+  return (evt.key === 'Escape');
+}
 
+/**
+ * Функция проверяет длину комментария.
+ * @param {String} comment - объект события.
+ * @return {boolean} - истина, если длина комментария укладывается в диапазон.
+ */
+function isCommentLengthCorrect (comment) {
+  return comment.length >= MIN_COMMENT_LENGTH && comment.length <= MAX_COMMENT_LENGTH;
+}
 
-form.addEventListener('submit', (evt) => {
-  if (!pristine.validate()) {
-    evt.preventDefault();
-  }
-});
+/**
+ * Функция устанавливает обработчики событий на окно редактора загруженного изображения.
+ */
+function addEventListenersToForm() {
+  uploadControl.addEventListener('change', openEditorModal);
+}
 
 export {addEventListenersToForm};
-
-//чистим обработчики событий и очищаем инпут формы загрузки
