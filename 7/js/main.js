48ad49f1d20615cd588bc388d26fb0ac060948c0
Keks <keks@htmlacademy.ru>
2022-10-23 15:19:37 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,7 @@
+import {createPhotos} from './mock/data.js';
+import {renderThumbnails} from './render-content.js';
+import {addEventListenersToForm} from'./form/form.js';
+
+const PHOTOS_COUNT = 25;
+renderThumbnails(createPhotos(PHOTOS_COUNT));
+addEventListenersToForm();
---
+++ 7/js/util.js
@@ -0,0 +1,11 @@
+/**
+ * Проверка длины строки
+ * @param {string} str — исходная строка
+ * @param {number} len — количество симолов для проверки
+ * @return {boolean} — истина, если строка в диапазоне
+ */
+function isStrLengthCorrect(str, len) {
+  return (str.length <= len);
+}
+
+export {isStrLengthCorrect};
