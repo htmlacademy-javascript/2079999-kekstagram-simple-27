ed89c9b5c16d8ef5c64ed59bd6e24c7bb03c0c65
Keks <keks@htmlacademy.ru>
2022-10-28 23:39:42 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/main.js
+++ 7/js/main.js
@@ -1,6 +1,6 @@
 import {createPhotos} from './mock/data.js';
 import {renderThumbnails} from './render-content.js';
-import {addEventListenersToForm} from'./form/form.js';
+import {addEventListenersToForm} from'./form.js';
 
 const PHOTOS_COUNT = 25;
 renderThumbnails(createPhotos(PHOTOS_COUNT));
