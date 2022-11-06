1d8753c826cccb48b05abd4b2982b11d9bb11b18
goregora <104126170+goregora@users.noreply.github.com>
2022-11-06 11:52:59 +0300

    Добавление получения/отправки данных на сервер + рефакторинг взаимодействия с формой

--- js/form.js
+++ js/form.js
@@ -1,3 +1,6 @@
+import {sendData} from './network.js';
+import {isEscapeKey} from './util.js';
+
 const ScaleValue = {
   MAX: 100,
   MIN: 25,
@@ -11,100 +14,136 @@ const СommentLength = {
 };
 
 /**
+   * Функция проверяет длину комментария.
+   * @param {String} comment - объект события.
+   * @return {boolean} - истина, если длина комментария укладывается в диапазон.
+   */
+function isCommentLengthCorrect (comment) {
+  return comment.length >= СommentLength.MIN && comment.length <= СommentLength.MAX;
+}
+
+/**
+ * Функция вызывает закрытие окна редактора загруженного изображения, если произошло нажатие ESC.
+ * @param {Object} evt - объект события.
+ */
+function EscapeKeydownHandler(evt) {
+  if (isEscapeKey(evt)) {
+    closeEditorModal();
+  }
+}
+
+const form = document.querySelector('.img-upload__form');
+const photoPreview = form.querySelector('.img-upload__preview');
+const scaleInputValue = form.querySelector('.scale__control--value');
+/**
+ * Функция устанавливает значение масштаба по умолчанию в окне редактора загруженного изображения.
+ */
+function scaleReset() {
+  scaleInputValue.value = `${ScaleValue.DEFAULT}%`;
+  photoPreview.style.transform = `scale(${ScaleValue.DEFAULT / 100})`;
+}
+
+const scaleUpButton = form.querySelector('.scale__control--bigger');
+const scaleDownButton = form.querySelector('.scale__control--smaller');
+/**
+   * Функция меняет значение масштаба в окне редактора загруженного изображения.
+   */
+function scaleChange(evt) {
+  let currentValue = Number((scaleInputValue.value).slice(0, -1));
+
+  if ((evt.target === scaleUpButton) && currentValue >= ScaleValue.MIN && currentValue < ScaleValue.MAX) {
+    scaleInputValue.value = `${currentValue + ScaleValue.STEP}%`;
+  }
+
+  if ((evt.target === scaleDownButton) && currentValue > ScaleValue.MIN && currentValue <= ScaleValue.MAX) {
+    scaleInputValue.value = `${currentValue - ScaleValue.STEP}%`;
+  }
+
+  currentValue = Number((scaleInputValue.value).slice(0, -1));
+  photoPreview.style.transform = `scale(${currentValue / 100})`;
+}
+
+const sliderDepthOfEffect = form.querySelector('.effect-level__slider');
+const sliderDepthOfEffectInput = form.querySelector('.effect-level__value');
+noUiSlider.create(sliderDepthOfEffect, {
+  range: {
+    min: 0,
+    max: 1
+  },
+  start: 1,
+  step: 0.1,
+  connect: 'lower',
+});
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__text',
+  errorTextParent: 'img-upload__text'
+});
+
+const editorModal = form.querySelector('.img-upload__overlay');
+/**
+ * Функция открывает окно редактора загруженного изображения.
+ */
+function openEditorModal() {
+  editorModal.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  sliderDepthOfEffect.setAttribute('disabled', true);
+  document.addEventListener('keydown', EscapeKeydownHandler);
+  scaleReset();
+}
+
+const effect = {
+  depth: 1,
+  get chrome() {
+    return `grayscale(${this.depth})`;
+  },
+  get sepia() {
+    return `sepia(${this.depth})`;
+  },
+  get marvin() {
+    return `invert(${this.depth * 100}%)`;
+  },
+  get phobos() {
+    return `blur(${this.depth * 3}px)`;
+  },
+  get heat() {
+    return `brightness(${this.depth * 3})`;
+  },
+  get none() {
+    return 'none';
+  }
+};
+
+const closeEditorModalButton = form.querySelector('#upload-cancel');
+const uploadControl = form.querySelector('#upload-file');
+let effectClass = 'none';
+/**
+ * Функция закрывает окно редактора загруженного изображения.
+ */
+function closeEditorModal() {
+  editorModal.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  uploadControl.value = '';
+  photoPreview.style.transform = `scale(${ScaleValue.DEFAULT / 100})`;
+  document.removeEventListener('keydown', EscapeKeydownHandler);
+  photoPreview.classList.remove(`effects__preview--${effectClass}`);
+  photoPreview.classList.add('effects__preview--none');
+  sliderDepthOfEffect.noUiSlider.set(1);
+  form.querySelector('#effect-none').checked = true;
+  effectClass = 'none';
+  photoPreview.style.filter = effect.none;
+}
+
+/**
  * Функция устанавливает обработчики событий на окно редактора загруженного изображения.
  */
 function addEventListenersToForm() {
-  const form = document.querySelector('.img-upload__form');
-
-  const uploadControl = form.querySelector('#upload-file');
   uploadControl.addEventListener('change', openEditorModal);
-
-  const closeEditorModalButton = form.querySelector('#upload-cancel');
   closeEditorModalButton.addEventListener('click', closeEditorModal);
-
-  const scaleDownButton = form.querySelector('.scale__control--smaller');
   scaleDownButton.addEventListener('click', scaleChange);
-
-  const scaleUpButton = form.querySelector('.scale__control--bigger');
   scaleUpButton.addEventListener('click', scaleChange);
-
-  const photoPreview = form.querySelector('.img-upload__preview');
   photoPreview.classList.add('effects__preview--none');
 
-  const scaleInputValue = form.querySelector('.scale__control--value');
-  /**
-   * Функция устанавливает значение масштаба по умолчанию в окне редактора загруженного изображения.
-   */
-  function scaleReset() {
-    scaleInputValue.value = `${ScaleValue.DEFAULT}%`;
-    photoPreview.style.transform = `scale(${ScaleValue.DEFAULT / 100})`;
-  }
-
-  /**
-   * Функция меняет значение масштаба в окне редактора загруженного изображения.
-   */
-  function scaleChange(evt) {
-    let currentValue = Number((scaleInputValue.value).slice(0, -1));
-
-    if ((evt.target === scaleUpButton) && currentValue >= ScaleValue.MIN && currentValue < ScaleValue.MAX) {
-      scaleInputValue.value = `${currentValue + ScaleValue.STEP}%`;
-    }
-
-    if ((evt.target === scaleDownButton) && currentValue > ScaleValue.MIN && currentValue <= ScaleValue.MAX) {
-      scaleInputValue.value = `${currentValue - ScaleValue.STEP}%`;
-    }
-
-    currentValue = Number((scaleInputValue.value).slice(0, -1));
-    photoPreview.style.transform = `scale(${currentValue / 100})`;
-  }
-
-  const sliderDepthOfEffect = form.querySelector('.effect-level__slider');
-  const sliderDepthOfEffectInput = form.querySelector('.effect-level__value');
-  noUiSlider.create(sliderDepthOfEffect, {
-    range: {
-      min: 0,
-      max: 1
-    },
-    start: 1,
-    step: 0.1,
-    connect: 'lower',
-  });
-
-  const editorModal = form.querySelector('.img-upload__overlay');
-  /**
-   * Функция открывает окно редактора загруженного изображения.
-   */
-  function openEditorModal() {
-    editorModal.classList.remove('hidden');
-    document.body.classList.add('modal-open');
-    sliderDepthOfEffect.setAttribute('disabled', true);
-    document.addEventListener('keydown', EscapeKeydownHandler);
-    scaleReset();
-  }
-
-  const effect = {
-    depth: 1,
-    get chrome() {
-      return `grayscale(${this.depth})`;
-    },
-    get sepia() {
-      return `sepia(${this.depth})`;
-    },
-    get marvin() {
-      return `invert(${this.depth * 100}%)`;
-    },
-    get phobos() {
-      return `blur(${this.depth * 3}px)`;
-    },
-    get heat() {
-      return `brightness(${this.depth * 3})`;
-    },
-    get none() {
-      return 'none';
-    }
-  };
-
-  let effectClass = 'none';
   sliderDepthOfEffect.noUiSlider.on('update', () => {
     effect.depth = sliderDepthOfEffect.noUiSlider.get();
     sliderDepthOfEffectInput.value = effect.depth;
@@ -127,59 +166,13 @@ function addEventListenersToForm() {
     }
   });
 
-  /**
-   * Функция закрывает окно редактора загруженного изображения.
-   */
-  function closeEditorModal() {
-    editorModal.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-    uploadControl.value = '';
-    photoPreview.style.transform = `scale(${ScaleValue.DEFAULT / 100})`;
-    document.removeEventListener('keydown', EscapeKeydownHandler);
-    photoPreview.classList.remove(`effects__preview--${effectClass}`);
-    photoPreview.classList.add('effects__preview--none');
-    sliderDepthOfEffect.noUiSlider.set(1);
-    form.querySelector('#effect-none').checked = true;
-    effectClass = 'none';
-    photoPreview.style.filter = effect.none;
-  }
-
-  /**
-   * Функция проверяет является ли нажатая клавиша ESC-ом.
-   * @param {Object} evt - объект события.
-   * @return {boolean} - истина, если нажатая клавиша - ESC.
-   */
-  function isEscapeKey(evt) {
-    return (evt.key === 'Escape');
-  }
-
-  /**
- * Функция вызывает закрытие окна редактора загруженного изображения, если произошло нажатие ESC.
- * @param {Object} evt - объект события.
- */
-  function EscapeKeydownHandler(evt) {
-    if (isEscapeKey(evt)) {
-      closeEditorModal();
-    }
-  }
-
-  /**
-   * Функция проверяет длину комментария.
-   * @param {String} comment - объект события.
-   * @return {boolean} - истина, если длина комментария укладывается в диапазон.
-   */
-  function isCommentLengthCorrect (comment) {
-    return comment.length >= СommentLength.MIN && comment.length <= СommentLength.MAX;
-  }
-
-  const pristine = new Pristine(form, {
-    classTo: 'img-upload__text',
-    errorTextParent: 'img-upload__text'
-  });
-
   form.addEventListener('submit', (evt) => {
     if (!pristine.validate()) {
       evt.preventDefault();
+    } else {
+      evt.preventDefault();
+      const formData = new FormData(evt.target);
+      sendData(formData);
     }
   });
 
--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-import {createPhotos} from './mock/data.js';
-import {renderThumbnails} from './render-content.js';
-import {addEventListenersToForm} from'./form.js';
+import {getData} from './network.js';
+import {addEventListenersToForm} from './form.js';
 
-const PHOTOS_COUNT = 25;
-renderThumbnails(createPhotos(PHOTOS_COUNT));
+getData();
 addEventListenersToForm();
