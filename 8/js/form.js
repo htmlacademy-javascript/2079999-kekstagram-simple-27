c5ed0d3ce06727721e306818fc99a3131da652a0
Keks <keks@htmlacademy.ru>
2022-11-09 18:58:50 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/form.js
+++ 8/js/form.js
@@ -26,9 +26,9 @@ function isCommentLengthCorrect (comment) {
  * Функция вызывает закрытие окна редактора загруженного изображения, если произошло нажатие ESC.
  * @param {Object} evt - объект события.
  */
-function EscapeKeydownHandler(evt) {
+function handleEscapeKeydown(evt) {
   if (isEscapeKey(evt)) {
-    closeEditorModal();
+    handleCloseEditorModal();
   }
 }
 
@@ -48,7 +48,7 @@ const scaleDownButton = form.querySelector('.scale__control--smaller');
 /**
    * Функция меняет значение масштаба в окне редактора загруженного изображения.
    */
-function scaleChange(evt) {
+function handleScaleChange(evt) {
   let currentValue = Number((scaleInputValue.value).slice(0, -1));
 
   if ((evt.target === scaleUpButton) && currentValue >= ScaleValue.MIN && currentValue < ScaleValue.MAX) {
@@ -75,23 +75,6 @@ noUiSlider.create(sliderDepthOfEffect, {
   connect: 'lower',
 });
 
-const pristine = new Pristine(form, {
-  classTo: 'img-upload__text',
-  errorTextParent: 'img-upload__text'
-});
-
-const editorModal = form.querySelector('.img-upload__overlay');
-/**
- * Функция открывает окно редактора загруженного изображения.
- */
-function openEditorModal() {
-  editorModal.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-  sliderDepthOfEffect.setAttribute('disabled', true);
-  document.addEventListener('keydown', EscapeKeydownHandler);
-  scaleReset();
-}
-
 const effect = {
   depth: 1,
   get chrome() {
@@ -114,45 +97,58 @@ const effect = {
   }
 };
 
-const closeEditorModalButton = form.querySelector('#upload-cancel');
+const editorModal = form.querySelector('.img-upload__overlay');
 const uploadControl = form.querySelector('#upload-file');
-let effectClass = 'none';
+let activeEffectClass = effect.none;
 /**
- * Функция закрывает окно редактора загруженного изображения.
+ * Функция открывает окно редактора загруженного изображения.
  */
-function closeEditorModal() {
-  editorModal.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-  uploadControl.value = '';
+function handleOpenEditorModal() {
+  editorModal.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  sliderDepthOfEffect.setAttribute('disabled', true);
+  document.addEventListener('keydown', handleEscapeKeydown);
+  scaleReset();
   photoPreview.style.transform = `scale(${ScaleValue.DEFAULT / 100})`;
-  document.removeEventListener('keydown', EscapeKeydownHandler);
-  photoPreview.classList.remove(`effects__preview--${effectClass}`);
+  photoPreview.classList.remove(`effects__preview--${activeEffectClass}`);
   photoPreview.classList.add('effects__preview--none');
   sliderDepthOfEffect.noUiSlider.set(1);
   form.querySelector('#effect-none').checked = true;
-  effectClass = 'none';
+  activeEffectClass = effect.none;
   photoPreview.style.filter = effect.none;
+  form.querySelector('.text__description').value = '';
+}
+
+const closeEditorModalButton = form.querySelector('#upload-cancel');
+/**
+ * Функция закрывает окно редактора загруженного изображения.
+ */
+function handleCloseEditorModal() {
+  editorModal.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', handleEscapeKeydown);
+  uploadControl.value = '';
 }
 
 /**
  * Функция устанавливает обработчики событий на окно редактора загруженного изображения.
  */
-function addEventListenersToForm() {
-  uploadControl.addEventListener('change', openEditorModal);
-  closeEditorModalButton.addEventListener('click', closeEditorModal);
-  scaleDownButton.addEventListener('click', scaleChange);
-  scaleUpButton.addEventListener('click', scaleChange);
+function addEventListenersToForm(onSuccess, onFail) {
+  uploadControl.addEventListener('change', handleOpenEditorModal);
+  closeEditorModalButton.addEventListener('click', handleCloseEditorModal);
+  scaleDownButton.addEventListener('click', handleScaleChange);
+  scaleUpButton.addEventListener('click', handleScaleChange);
   photoPreview.classList.add('effects__preview--none');
 
   sliderDepthOfEffect.noUiSlider.on('update', () => {
     effect.depth = sliderDepthOfEffect.noUiSlider.get();
     sliderDepthOfEffectInput.value = effect.depth;
-    photoPreview.style.filter = effect[effectClass];
+    photoPreview.style.filter = effect[activeEffectClass];
   });
 
   form.addEventListener('change', (evt) => {
     if (evt.target.name === 'effect') {
-      photoPreview.classList.remove(`effects__preview--${effectClass}`);
+      photoPreview.classList.remove(`effects__preview--${activeEffectClass}`);
       photoPreview.classList.add(`effects__preview--${evt.target.value}`);
       sliderDepthOfEffect.noUiSlider.set(1);
       photoPreview.style.filter = effect[evt.target.value];
@@ -162,25 +158,29 @@ function addEventListenersToForm() {
       } else {
         sliderDepthOfEffect.setAttribute('disabled', true);
       }
-      effectClass = evt.target.value;
+      activeEffectClass = evt.target.value;
     }
   });
 
-  form.addEventListener('submit', (evt) => {
-    if (!pristine.validate()) {
-      evt.preventDefault();
-    } else {
-      evt.preventDefault();
-      const formData = new FormData(evt.target);
-      sendData(formData);
-    }
+  const pristine = new Pristine(form, {
+    classTo: 'img-upload__text',
+    errorTextParent: 'img-upload__text'
   });
 
   pristine.addValidator(
     form.querySelector('.text__description'),
     isCommentLengthCorrect,
-    `от ${СommentLength.MIN} до ${СommentLength.MAX} символов`
+    `Комментарий должен содержать от ${СommentLength.MIN} до ${СommentLength.MAX} символов`
   );
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    if (pristine.validate()) {
+      sendData(onSuccess, onFail, new FormData(evt.target));
+    } else {
+      onFail();
+    }
+  });
 }
 
-export {addEventListenersToForm};
+export {addEventListenersToForm, handleCloseEditorModal as closeEditorModal};
